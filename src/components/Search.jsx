e819e175294c6e0d19bc06a18c88e4c051af0c27
mihaiBondoc <bondoc.mihail@gmail.com>
2022-10-30 21:18:26 +0200

    30 oct 2022

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -34,7 +34,7 @@ const FormStyle = styled.form`
   }
   input {
     border: none;
-    background: linear-gradient(35deg, #494949, #313131);
+    background: linear-gradient(36deg, #494949, #313131);
     font-size: 1.5rem;
     color: white;
     padding: 1rem 3rem;
